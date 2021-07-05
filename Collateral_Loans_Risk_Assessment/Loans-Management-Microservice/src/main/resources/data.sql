1a25ef00f71d8271b5246208a99a3108786e9656
sravanimaddela <sravanimaddela98@gmail.com>
2021-07-06 00:24:56 +0530

    CDE Enhancement Project - Collateral Loans Risk Assessment

---
+++ Collateral_Loans_Risk_Assessment/Loans-Management-Microservice/src/main/resources/data.sql
@@ -0,0 +1,32 @@
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100001,'Vinay Sharma','vinay@gmail.com','7365907119','Mumbai');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100002,'Pavan Kumar','pavan@gmail.com','9874561231','Karnataka');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100003,'Sanjay Reddy','sanjay@gmail.com','4567891232','Chennai');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100004,'Chandu kumar','chandu@gmail.com','1569891232','Hyderabad');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100005,'Raga Sindhuja','sindhu@gmail.com','3698891232','Tamilnadu');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100006,'Laxmi Lavanya','laxmi@gmail.com','6985891232','Kerala');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100007,'sravani maddela','sravani@gmail.com','4567891232','Vijayawada');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100008,'sravani maddela','sravani@gmail.com','4567891232','Vijayawada');
+
+ 
+
+ 
+
+ 
+
+
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1001,'Home Loan',1500000,12.5,36,'REAL_ESTATE');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1002,'Home Loan',1000000,13.5,30,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1003,'Car Loan',700000,11.5,30,'REAL_ESTATE');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1004,'Car Loan',500000,12,24,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1005,'Education Loan',500000,12,24,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1006,'Medical Loan',500000,12,24,'REAL_ESTATE');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1007,'Home Loan',500000,12,24,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1008,'Medical Loan',500000,12,24,'CASH_DEPOSIT');
+
+ 
+
+ 
+
+ 
+
+
---
+++ Collateral_Loans_Risk_Assessment/authorization-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(id, name, password) values (1, 'Sravani', 'admin');
+
+
+
+
+
+insert into customer(id,name,password) values (100011,'sharanya','sharanya');
+insert into customer(id,name,password) values (100012,'vijaya','vijaya');
